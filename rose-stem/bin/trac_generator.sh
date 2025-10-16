a5cad1743592d96e0fd45801782742675ad54d4c
Theo Geddes <108924122+mo-tgeddes@users.noreply.github.com>
2025-10-16 14:24:17 +0100

    14: Plug in task_status code and simplify existing code

--- rose-stem/bin/trac_generator.sh
+++ rose-stem/bin/trac_generator.sh
@@ -9,32 +9,29 @@ set -eu
 
 export DB_LOCATION=${CYLC_WORKFLOW_RUN_DIR}/log/db
 export OUTPUT_STATUS_LOG=${CYLC_WORKFLOW_RUN_DIR}/trac_status.log
+export TASK_STATUS=${CYLC_WORKFLOW_RUN_DIR}/task_status.log
 export OUTPUT_DURATION_LOG=${CYLC_WORKFLOW_RUN_DIR}/trac_durations.log
 
 # Suppress ants-launch debug logs
 export QUIET_MODE=true
 
-# Put ants-launch and durations_main.py on the path
+# Put ants-launch, durations_main.py and generate_ants_test_logs.py on the path
 export PATH=${CYLC_WORKFLOW_SHARE_DIR}/fcm_make_ants/build/bin/:$PATH
 export PATH=${CYLC_WORKFLOW_SHARE_DIR}/fcm_make_ants/build/utils/generate_logs/:$PATH
+export PATH=${CYLC_WORKFLOW_SHARE_DIR}/fcm_make_ants/build/utils/task_status/:$PATH
 
 # User info for OUTPUT_STATUS_LOG
 date > $OUTPUT_STATUS_LOG
 echo "-----" >> $OUTPUT_STATUS_LOG
-echo $USER ran suite located at: $CYLC_WORKFLOW_RUN_DIR >> $OUTPUT_STATUS_LOG
-echo "-----" >> $OUTPUT_STATUS_LOG
-
-# Verson info for OUTPUT_STATUS_LOG
-echo "=== Revision information ===" >> $OUTPUT_STATUS_LOG
-
-echo "{{{" >> $OUTPUT_STATUS_LOG
-cat ${CYLC_WORKFLOW_RUN_DIR}/log/version/vcs.json >> $OUTPUT_STATUS_LOG
-echo -e "\n}}}" >> $OUTPUT_STATUS_LOG
 
 # Generate test status table for OUTPUT_STATUS_LOG
-echo "=== Test Results ===" >> $OUTPUT_STATUS_LOG
-echo " || '''task''' || '''status''' || " >> $OUTPUT_STATUS_LOG
-ants-launch sqlite3 -separator " || " $DB_LOCATION "select '', name, status, '' from task_states" >> $OUTPUT_STATUS_LOG
+echo "## Test Results ##" >> $OUTPUT_STATUS_LOG
+echo " | **task** | **status** | " >> $OUTPUT_STATUS_LOG
+echo "|:-|:-|" >> $OUTPUT_STATUS_LOG
+ants-launch sqlite3 -separator " | " $DB_LOCATION "select '', name, status, '' from task_states" >> $OUTPUT_STATUS_LOG
+
+# Generate TASK_STATUS log
+ants-launch generate_ants_test_logs.py $DB_LOCATION $TASK_STATUS gh
 
 # Generate OUTPUT_DURATION_LOG
 ants-launch durations_main.py $DB_LOCATION $OUTPUT_DURATION_LOG github
