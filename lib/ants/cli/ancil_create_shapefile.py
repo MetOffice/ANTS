e47273e37066dd067fe885e6113107b89f64ec20
Alasdair Roy <292044538+alasdair-roy@users.noreply.github.com>
2026-07-09 10:38:52 +0100

    Placed sketch solution for rotating co-ordinate systems

--- lib/ants/cli/ancil_create_shapefile.py
+++ lib/ants/cli/ancil_create_shapefile.py
@@ -13,7 +13,9 @@ points defining a single polygon in a specified polygon file.
 import argparse
 import json
 
+import cartopy.crs as ccrs
 import numpy as np
+from ants.io.load import load_landsea_mask
 from osgeo import ogr
 from shapely.geometry import Polygon
 
@@ -39,7 +41,49 @@ def _load_polygon_from_json(json_file):
     return polygon
 
 
-def main(json_file, output):
+def _load_data(target_lsm_path):
+    """
+    Loads in the target lsm which defines the rotated pole coordinate system
+    which is used as the new reference coordinate system for the shapefile.
+
+    Parameters
+    ----------
+    target_lsm_path : str
+        Path to the lsm file.
+
+    Returns
+    -------
+
+    """
+
+    target_lsm = load_landsea_mask(target_lsm_path)
+
+    return target_lsm
+
+
+def _rotate_domains(target_lsm, lon, lat):
+    """
+    Testing could consist of rotating through obvious angles
+    One set where the say the new pole is just rotated to the equator
+
+    Two approaches are available:
+        1. create an iris cube with the lat lon pairs
+        2. just create a reference crs
+    """
+    source_crs = ccrs.PlateCarree()
+    rotated_points = (
+        target_lsm.coord_system()
+        .as_cartopy_crs()
+        .transform_points(
+            source_crs,  # source_crs
+            lon,
+            lat,
+        )
+    )
+    return rotated_points
+
+
+def main(json_file, output, target_lsm_path):
     """
     Loads in a provided json file that defines pairs of longitude, latitude
     points to create a polygon from. That polygon is then used to create a
@@ -51,11 +95,20 @@ def main(json_file, output):
         Path to json file
     output : str
         Location to store generated shape file
-
+    target_lsm_path : str
+        File path for a land sea mask that provides the new rotated pole
+        coordinates to which the data be mapped.
     """
 
     # Load a json and make a polygon
     polygon = _load_polygon_from_json(json_file)
+
+    # TODO replace with lat lon pairs from polygon file
+    lon, lat = [], []
+    lsm = _load_data(target_lsm_path)
+    rotated_points = _rotate_domains(lsm, lon, lat)
+    # TODO assign rotated points to polygon (this is a placeholder for commit)
+    rotated_points = rotated_points + 1
 
     # Now convert it to a shapefile with OGR
     driver = ogr.GetDriverByName("Esri Shapefile")
@@ -82,7 +135,9 @@ def main(json_file, output):
 
 
 def _get_parser():
-    parser = argparse.ArgumentParser()
+    parser = argparse.ArgumentParser(
+        target_lsm=True,
+    )
     parser.add_argument(
         "json_file", help="Path to json file defining polygon to generate."
     )
@@ -93,7 +148,7 @@ def _get_parser():
 def cli_interface():
     parser = _get_parser()
     args = parser.parse_args()
-    main(args.json_file, args.output)
+    main(args.json_file, args.output, args.target_lsm)
 
 
 if __name__ == "__main__":
