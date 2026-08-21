b76caf7a6a069e012f03e6479b6c9cea69087902
Alasdair Roy <292044538+alasdair-roy@users.noreply.github.com>
2026-08-21 13:28:17 +0100

    Added new ite test and made other tests more consistent.

--- lib/ants/tests/cli/test_ancil_create_shapefile.py
+++ lib/ants/tests/cli/test_ancil_create_shapefile.py
@@ -47,21 +47,21 @@ class Test__check_polygon_validity(ants.tests.TestCase):
         """Test that an invalid polygon with intersecting boundaries
         raises an error."""
 
-        self.invalid_points = np.array([[1, 1], [-1, -1], [1, -1], [-1, 1]])
-        self.invalid_poly = Polygon(self.invalid_points)
-        self.is_ccw = False
+        invalid_points = np.array([[1, 1], [-1, -1], [1, -1], [-1, 1]])
+        invalid_poly = Polygon(invalid_points)
+        is_ccw = False
 
         error_msg = "Polygon is invalid: "
 
         with self.assertRaisesRegex(ValueError, error_msg):
-            _check_polygon_validity(self.invalid_poly, self.is_ccw)
+            _check_polygon_validity(invalid_poly, is_ccw)
 
     def test_changed_orientation(self):
         """Test that a change in polygon orientation raises an error."""
 
-        self.ccw_points = np.array([[1, 1], [-1, 1], [-1, -1], [1, -1]])
-        self.ccw_poly = Polygon(self.ccw_points)
-        self.is_ccw = False
+        ccw_points = np.array([[1, 1], [-1, 1], [-1, -1], [1, -1]])
+        ccw_poly = Polygon(ccw_points)
+        is_ccw = False
 
         error_msg = (
             "Polygon orientation has changed. Expected is_ccw=False, "
@@ -69,7 +69,7 @@ class Test__check_polygon_validity(ants.tests.TestCase):
         )
 
         with self.assertRaisesRegex(ValueError, error_msg):
-            _check_polygon_validity(self.ccw_poly, self.is_ccw)
+            _check_polygon_validity(ccw_poly, is_ccw)
 
 
 class Test__validate_orientation(ants.tests.TestCase):
@@ -83,128 +83,107 @@ class Test__validate_orientation(ants.tests.TestCase):
         """Test that a warning is raised with a coordinate system at latitude=90.0,
         longitude=0.0 is passed. Check the function returns False."""
 
-        self.sphere_identity_target_lsm = CubeBuilder(
-            self.sphere_identity_crs, (2, 2)
-        )._cube
+        target_lsm = CubeBuilder(self.sphere_identity_crs, (2, 2))._cube
 
-        self.warning_msg = (
+        warning_msg = (
             "target_lsm has a geodetic coordinate system with pole located"
             " at grid_longitude=0.0, grid_latitude=90.0."
             " No transformation will be carried out."
         )
 
-        with self.assertWarnsRegex(UserWarning, self.warning_msg):
-            valid_crs = _validate_orientation(self.sphere_identity_target_lsm)
+        with self.assertWarnsRegex(UserWarning, warning_msg):
+            valid_crs = _validate_orientation(target_lsm)
         self.assertFalse(valid_crs)
 
     def test_true_returned(self):
         """Test that True is returned when target_lsm has a valid rotated pole."""
 
-        self.sphere_rotate_lon = geodetic(
+        target_lsm = geodetic(
             (2, 2), north_pole_lat=90.0, north_pole_lon=90.0, crs=self.sphere_crs
         )
 
-        self.assertTrue(_validate_orientation(self.sphere_rotate_lon))
+        self.assertTrue(_validate_orientation(target_lsm))
 
     def test_no_warning_raised_rotated(self):
         """Test that passing a non-zero central rotated longitude is still
         accounted for when checking if the coordinate system is rotated."""
 
-        self.sphere_rotated_crs = iris.coord_systems.RotatedGeogCS(
+        crs = iris.coord_systems.RotatedGeogCS(
             90.0, 0.0, 180.0, ellipsoid=self.sphere_crs
         )
-        self.sphere_rotated_target_lsm = CubeBuilder(
-            self.sphere_rotated_crs, (2, 2)
-        )._cube
+        target_lsm = CubeBuilder(crs, (2, 2))._cube
 
-        self.assertTrue(_validate_orientation(self.sphere_rotated_target_lsm))
+        self.assertTrue(_validate_orientation(target_lsm))
 
 
 class Test__transform_coordinates(ants.tests.TestCase):
     def setUp(self):
         self.sphere_crs = iris.coord_systems.GeogCS(6371229.0)
-        self.sphere_identity_crs = iris.coord_systems.RotatedGeogCS(
-            90.0, 0.0, ellipsoid=self.sphere_crs
-        )
         self.sphere_source = CubeBuilder(self.sphere_crs, (2, 2))._cube
 
     def test_identity_rotation_sphere(self):
         """Test that rotation to a pole at latitude=90.0, longitude=0.0
-        returns the same points, except a 180.0 degree rotation in longitude. By
-        convention the new pole is defined 180.0 rotated from the provided
-        longitude."""
+        returns the same points.
 
-        self.sphere_identity_target_lsm = CubeBuilder(
-            self.sphere_identity_crs, (2, 2)
-        )._cube
+        By convention, rotated pole co-ordinate systems will set the prime
+        meridian rotated 180.0 from the specified longitude. To place the
+        prime meridian at 0.0, we apply a further rotation of 180.0, following
+        rotation to the new pole.
+        """
 
-        self.vary_latitudes = np.array(
-            [[0.0, 0.0, 0.0, 0.0, 0.0], [90.0, 45.0, 0.0, -45.0, -90.0]]
-        ).T
-        true_lats = np.array([90, 45, 0, -45, -90])
-        true_lons = np.array([np.nan, -180.0, -180.0, -180.0, -180.0])
-        expected_rotation = np.array([true_lons, true_lats]).T
-
-        # We cannot check the longitude at the pole (any longitude is valid).
-        check_mask = ~np.isnan(expected_rotation)
-
-        rotated_coords = _transform_coordinates(
-            self.sphere_identity_target_lsm, self.sphere_source, self.vary_latitudes
+        crs = iris.coord_systems.RotatedGeogCS(
+            90.0, 0.0, 180.0, ellipsoid=self.sphere_crs
         )
-        self.assertArrayAlmostEqual(
-            expected_rotation[check_mask], rotated_coords[check_mask]
-        )
+        target_lsm = CubeBuilder(crs, (2, 2))._cube
 
-    # TODO this test doesn't really make sense. These are really just a
-    # line of points and not a polygon.
+        points = np.array([[10, 10], [10, -10], [-10, -10], [-10, 10]])
+
+        expected_points = np.copy(points)
+
+        rotated_coords = _transform_coordinates(target_lsm, self.sphere_source, points)
+        self.assertArrayAlmostEqual(expected_points, rotated_coords)
+
     def test_longitudinal_rotation(self):
-        """Test rotation to a new pole at latitude=90.0, longitude=90.0.
-        The new pole is located at latitude=90.0, longitude=270.0 in the rotated
-        pole coordinate system."""
+        """Test rotation to a new pole at latitude=90.0, longitude=90.0."""
 
-        self.sphere_rotate_lon = geodetic(
-            (2, 2), north_pole_lat=90.0, north_pole_lon=90.0, crs=self.sphere_crs
+        crs = iris.coord_systems.RotatedGeogCS(
+            90.0, 90.0, 180.0, ellipsoid=self.sphere_crs
+        )
+        target_lsm = CubeBuilder(crs, (2, 2))._cube
+
+        points = np.array([[10, 10], [10, -10], [-10, -10], [-10, 10]])
+        expected_points = np.array(
+            [[-80.0, 10.0], [-80.0, -10.0], [-100.0, -10.0], [-100.0, 10]]
         )
 
-        points = np.array([[0.0, 45.0, 90.0, 135.0], [0.0, 0.0, 0.0, 0.0]]).T
-        true_lats = np.array([0.0, 0.0, np.nan, 0.0])
-        true_lons = np.array([90.0, 135.0, 180.0, 225.0])
-        expected_rotation = np.array([true_lons, true_lats]).T
+        rotated_coords = _transform_coordinates(target_lsm, self.sphere_source, points)
 
-        check_mask = ~np.isnan(expected_rotation)
+        self.assertArrayAlmostEqual(expected_points, rotated_coords)
 
-        rotated_coords = _transform_coordinates(
-            self.sphere_rotate_lon, self.sphere_source, points
+    def test_antimeridian_rotation(self):
+        """Test rotation to a new pole at latitude=90.0, longitude=180.0."""
+
+        crs = iris.coord_systems.RotatedGeogCS(
+            90.0, 180.0, 180.0, ellipsoid=self.sphere_crs
+        )
+        target_lsm = CubeBuilder(crs, (2, 2))._cube
+
+        points = np.array([[10, 10], [10, -10], [-10, -10], [-10, 10]])
+        expected_points = np.array(
+            [[190.0, 10.0], [190.0, -10.0], [170.0, -10.0], [170.0, 10]]
         )
 
-        self.assertArrayAlmostEqual(
-            expected_rotation[check_mask], rotated_coords[check_mask]
+        warning_msg = re.escape(
+            "The transformed points are assumed to cross the antimeridian. "
+            "The longitudinal points will instead be defined in the interval"
+            " [0, 360.0] degrees."
         )
+        with self.assertWarnsRegex(UserWarning, warning_msg):
+            rotated_coords = _transform_coordinates(
+                target_lsm, self.sphere_source, points
+            )
 
-    def test_rotation_to_equator_sphere(self):
-        """Test rotation to a new pole at latitude=0.0, longitude=0.0."""
-        self.vary_longitudes = np.array(
-            [
-                [-135.0, -90.0, -45.0, 0.0, 45.0, 90.0, 135.0],
-                [0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0],
-            ]
-        ).T
-
-        self.sphere_equator_target_lsm = geodetic(
-            (2, 2), north_pole_lat=0.0, north_pole_lon=0.0, crs=self.sphere_crs
-        )
-
-        true_lats = np.array([-45.0, 0.0, 45.0, 90.0, 45.0, 0.0, -45.0])
-        true_lons = np.array([90.0, 90.0, 90.0, np.nan, -90.0, -90.0, -90.0])
-        expected_rotation = np.array([true_lons, true_lats]).T
-        check_mask = ~np.isnan(expected_rotation)
-
-        rotated_coords = _transform_coordinates(
-            self.sphere_equator_target_lsm, self.sphere_source, self.vary_longitudes
-        )
-        self.assertArrayAlmostEqual(
-            expected_rotation[check_mask], rotated_coords[check_mask]
-        )
+        self.assertArrayAlmostEqual(expected_points, rotated_coords)
 
 
 class Test__validate_args(ants.tests.TestCase):
@@ -264,16 +243,13 @@ class Test__transform_if_required(ants.tests.TestCase):
     def test_non_rotated_returned(self):
         """Test that the input points are returned if a non-rotated pole is
         provided as the target_lsm."""
-        self.sphere_identity_crs = iris.coord_systems.RotatedGeogCS(
-            90.0, 0.0, ellipsoid=self.sphere_crs
-        )
-        self.sphere_identity_target_lsm = CubeBuilder(
-            self.sphere_identity_crs, (2, 2)
-        )._cube
+
+        crs = iris.coord_systems.RotatedGeogCS(90.0, 0.0, ellipsoid=self.sphere_crs)
+        target_lsm = CubeBuilder(crs, (2, 2))._cube
 
         with self.assertWarns(UserWarning):
             rotated_points = _transform_if_required(
-                self.sphere_identity_target_lsm, self.sphere_source, self.points
+                target_lsm, self.sphere_source, self.points
             )
 
         self.assertArrayEqual(rotated_points, self.points)
@@ -282,21 +258,19 @@ class Test__transform_if_required(ants.tests.TestCase):
         """Test that transform coordinates is called with the correct arguments
         when a valid target_lsm is given."""
 
-        self.sphere_equator_target_lsm = geodetic(
-            (2, 2), north_pole_lat=0.0, north_pole_lon=0.0, crs=self.sphere_crs
-        )
+        crs = iris.coord_systems.RotatedGeogCS(0.0, 0.0, ellipsoid=self.sphere_crs)
+        target_lsm = CubeBuilder(crs, (2, 2))._cube
+
         with mock.patch(
             "ants.cli.ancil_create_shapefile._transform_coordinates"
         ) as mock_transform:
-            _ = _transform_if_required(
-                self.sphere_equator_target_lsm, self.sphere_source, self.points
-            )
+            _ = _transform_if_required(target_lsm, self.sphere_source, self.points)
 
-        target_lsm, source, points = mock_transform.call_args.args
+        received_lsm, received_source, received_points = mock_transform.call_args.args
         mock_transform.assert_called_once()
-        self.assertEqual(target_lsm, self.sphere_equator_target_lsm)
-        self.assertEqual(source, self.sphere_source)
-        self.assertArrayEqual(points, self.points)
+        self.assertEqual(target_lsm, received_lsm)
+        self.assertEqual(received_source, self.sphere_source)
+        self.assertArrayEqual(received_points, self.points)
 
 
 class Test_ite_transform(ants.tests.TestCase):
@@ -310,7 +284,7 @@ class Test_ite_transform(ants.tests.TestCase):
         The points have been obtained by unrotating the coordinates in
         $UMDIR/ancil/data/shapefiles/ite_ukv_polygon/runme.py
         """
-        self.ite_points = np.array(
+        points = np.array(
             [
                 [1.63160953, 51.09703216],
                 [-0.29556461, 50.36332573],
@@ -331,7 +305,7 @@ class Test_ite_transform(ants.tests.TestCase):
             ]
         )
 
-        self.expected_rotated_points = np.array(
+        expected_points = np.array(
             [
                 [362.594, -1.32876],
                 [361.407, -2.1152],
@@ -351,16 +325,14 @@ class Test_ite_transform(ants.tests.TestCase):
                 [362.666, -1.18577],
             ]
         )
-        self.sphere_crs = iris.coord_systems.GeogCS(6371229.0)
-        self.sphere_source = CubeBuilder(self.sphere_crs, (2, 2))._cube
+        sphere_crs = iris.coord_systems.GeogCS(6371229.0)
+        source = CubeBuilder(sphere_crs, (2, 2))._cube
+        crs = iris.coord_systems.RotatedGeogCS(37.5, 177.5, ellipsoid=sphere_crs)
+        target_lsm = CubeBuilder(crs, (2, 2))._cube
 
-        target_lsm = geodetic(
-            (2, 2), north_pole_lat=37.5, north_pole_lon=177.5, crs=self.sphere_crs
-        )
+        rotated_points = _transform_if_required(target_lsm, source, points)
 
-        points = _transform_if_required(target_lsm, self.sphere_source, self.ite_points)
+        # Adding 360.0 to longitude to compare to the expected points
+        rotated_points[:, 0] += 360.0
 
-        # Adding 360.0 to compare to the expected points
-        points[:, 0] += 360.0
-
-        self.assertArrayAlmostEqual(points, self.expected_rotated_points)
+        self.assertArrayAlmostEqual(rotated_points, expected_points)
